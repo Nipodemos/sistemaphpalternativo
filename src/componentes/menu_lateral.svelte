e1e3a02b4aea0aed203ad39f50889baa0016d06f
Nipodemos <nipodemos13@gmail.com>
2024-06-04 22:41:39 -0300

    cria menu lateral

---
+++ src/componentes/menu_lateral.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { MenuLateral } from '$lib/types';
+	export let menus: MenuLateral;
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+</script>
+
+<Accordion>
+	{#each Object.keys(menus) as nomeMenu}
+		<AccordionItem>
+			<svelte:fragment slot="lead"><i class={menus[nomeMenu][0].icone}></i></svelte:fragment>
+			<svelte:fragment slot="summary">{nomeMenu}</svelte:fragment>
+			<svelte:fragment slot="content">
+				<nav class="list-nav">
+					<ul>
+						{#each menus[nomeMenu] as dadosMenu}
+							<li>
+								<a href={dadosMenu.url}>
+									<span class="badge bg-primary-500"><i class="fa-solid fa-arrow-right"></i></span>
+									<span class="flex-auto">{dadosMenu.submenu}</span>
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</nav>
+			</svelte:fragment>
+		</AccordionItem>
+	{/each}
+</Accordion>
