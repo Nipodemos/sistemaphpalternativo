71643b2f4ad2b236958421c86e03c20b07f84275
Nipodemos <nipodemos13@gmail.com>
2024-06-10 23:52:16 -0300

    Criando a página listar_cliente só pra ter o botão para o cadastrar_cliente

---
+++ src/routes/cliente/listar_cliente/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="row">
+	<div class="col-12 text-right">
+		<a href="/cliente/cadastrar_cliente" class="btn-icon variant-filled">
+			<i class="fa-solid fa-plus"></i>
+		</a>
+	</div>
+</div>
