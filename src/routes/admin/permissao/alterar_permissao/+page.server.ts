5ab21e46f82725caf61bf9335a3a200d5aedfb83
Nipodemos <nipodemos13@gmail.com>
2024-10-13 23:05:05 -0300

    agora finalmente a tela de permissões pelo menos mostra as permissões na tela, agora falta salvar

--- src/routes/admin/permissao/alterar_permissao/+page.server.ts
+++ src/routes/admin/permissao/alterar_permissao/+page.server.ts
@@ -1,39 +1,20 @@
 import { getDb } from '$lib/database/connection';
-import type { Funcionario, PermissaoTela } from '$lib/database/types';
-import { jsonify, type Jsonify } from 'surrealdb';
+import type { Funcionario, Tela } from '$lib/database/types';
+import { jsonify, StringRecordId } from 'surrealdb';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
 	const telaID = url.searchParams.get('id');
 	if (!telaID) return { status: 404 };
 	const db = getDb();
-	//let array = db.query();
 
 	let funcionarios = await db.select<Funcionario>('funcionario');
 	funcionarios = jsonify<Funcionario[]>(funcionarios);
-	let permissoes = await db.select<PermissaoTela>('permissaoTela');
-	permissoes = jsonify<PermissaoTela[]>(permissoes);
-	const nomeDaTela = permissoes[0].tela.menu;
-	const permissoesPorFuncionario = unificarFuncionariosPermissoes(funcionarios, permissoes);
-	return { permissoesPorFuncionario, nomeDaTela };
-};
+	console.log('telaID :>> ', telaID);
 
-const unificarFuncionariosPermissoes = (
-	funcionarios: Jsonify<Funcionario[]>,
-	permissoes: Jsonify<PermissaoTela[]>
-) => {
-	return funcionarios.map((funcionario) => {
-		// Filtra as permissões do funcionário atual
-		const permissoesFuncionario = permissoes
-			.filter((permissao) => permissao.funcionario.id === funcionario.id)
-			.reduce((acumulado: { [key: string]: string[] }, permissao) => {
-				const nomeTela = permissao.tela.id as unknown as string;
-				if (!acumulado[nomeTela]) acumulado[nomeTela] = [];
-				acumulado[nomeTela].push(permissao.permissao);
-				return acumulado;
-			}, {});
+	let dadosDaTela = await db.select<Tela>(new StringRecordId(telaID));
+	dadosDaTela = jsonify<Tela>(dadosDaTela);
+	console.log('tela :>> ', dadosDaTela);
 
-		// Adiciona as permissões ao objeto do funcionário
-		return { ...funcionario, permissoes: permissoesFuncionario };
-	});
+	return { funcionarios, dadosDaTela };
 };
--- src/routes/admin/permissao/alterar_permissao/+page.svelte
+++ src/routes/admin/permissao/alterar_permissao/+page.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
+	import type { Funcionario } from '$lib/database/types.js';
+	import { SlideToggle } from '@skeletonlabs/skeleton';
+
 	export let data;
-	const permissoesPorFuncionario = data.permissoesPorFuncionario;
+	const funcionarios = data.funcionarios!;
+	const dadosDaTela = data.dadosDaTela!;
+	let permissoes = ['Visualizar', 'Inserir', 'Alterar', 'Excluir', 'Relatório'];
+	const temPermissao = (perm: string, func: Funcionario) => {
+		if (data.dadosDaTela) {
+			let tela = func.permissoesTela.find((p) => p.tela.id == data.dadosDaTela.id);
+			if (tela) {
+				let permissao = tela.permissoes.find((p) => p === perm);
+				if (permissao) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	};
 </script>
 
 <div class="flex">
 	<div class="mb-8">
-		<h2 class="h2">Editar permissões da tela</h2>
+		<h3 class="h3">Editar permissões da tela de {dadosDaTela.submenu}</h3>
 	</div>
 </div>
 
@@ -14,33 +32,23 @@
 	<table class="table table-hover">
 		<thead>
 			<tr>
-				<th>Menu</th>
-				<th>Submenu</th>
-				<th>Ações</th>
+				<th>Funcionário</th>
+				{#each permissoes as permissao}
+					<th>{permissao}</th>
+				{/each}
 			</tr>
 		</thead>
 		<tbody>
-			{#each telasFiltradas as tela}
+			{#each funcionarios as funcionario}
 				<tr>
-					<td>{tela.menu}</td>
-					<td>{tela.submenu}</td>
-					<td class="text-center">
-						<a
-							href="/admin/permissao/alterar_permissao?id={tela.id}"
-							class="btn-icon variant-filled"
-						>
-							<i class="fa fa-pencil"></i>
-						</a>
-					</td>
+					<td>{funcionario.nome}</td>
+					{#each permissoes as p}
+						<td class="text-left">
+							<SlideToggle name={funcionario.id + ';' + p} checked={temPermissao(p, funcionario)} />
+						</td>
+					{/each}
 				</tr>
 			{/each}
 		</tbody>
-		<tfoot>
-			<tr>
-				<th>Menu</th>
-				<th>Submenu</th>
-				<th>Ações</th>
-			</tr>
-		</tfoot>
 	</table>
 </div>
