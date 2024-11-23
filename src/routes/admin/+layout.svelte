ec63761a4d65fee6afdd52a9e9cb344ee17bc158
Nipodemos <nipodemos13@gmail.com>
2024-11-23 14:44:39 -0300

    usando novo layout do skeleton 3, incompleto

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
 	import '../../app.css';
-	import { AppShell } from '@skeletonlabs/skeleton';
-	import { AppBar } from '@skeletonlabs/skeleton';
 	import type { AfterNavigate } from '@sveltejs/kit';
 	import { afterNavigate } from '$app/navigation';
 
 	import { page } from '$app/stores';
-	import MenuLateral from '../../componentes/menu_lateral.svelte';
-	import { ToastProvider } from '@skeletonlabs/skeleton-svelte';
+	// import MenuLateral from '../../componentes/menu_lateral.svelte';
+	import { AppBar, ToastProvider } from '@skeletonlabs/skeleton-svelte';
 
 	const url = $page.url.pathname;
-	export let data;
+	let { children, data } = $props();
 	let menus = data.menus;
 
 	afterNavigate((params: AfterNavigate) => {
@@ -20,31 +18,35 @@
 			elemPage.scrollTop = 0;
 		}
 	});
-
 </script>
 
 <ToastProvider>
-	{@render children()}
-</ToastProvider>
-<AppShell slotSidebarLeft="w-60" slotPageContent="p-4 bg-slate-400">
-	<svelte:fragment slot="header">
-		<AppBar>
-			<svelte:fragment slot="lead"><i class="fa-solid fa-user"></i></svelte:fragment>
-			EVG Sistemas
-			<svelte:fragment slot="trail">(actions)</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-
-	<svelte:fragment slot="sidebarLeft">
-		<MenuLateral {menus} />
-	</svelte:fragment>
-	<!-- (sidebarRight) -->
-	<!-- (pageHeader) -->
-	<!-- Router Slot -->
-	<div class="card p-4 container">
-		<slot />
+	<div class="grid h-screen grid-rows-[auto_1fr_auto]">
+		<!-- Header -->
+		<header class="bg-red-500 p-4">
+			<AppBar>
+				{#snippet lead()}
+					<i class="fa-solid fa-arrow-left"></i>
+					EVG Sistemas
+				{/snippet}
+				{#snippet trail()}
+					menu pra sair
+				{/snippet}
+				<span>Title</span>
+			</AppBar>
+		</header>
+		<!-- Grid Columns -->
+		<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
+			<!-- Left Sidebar. -->
+			<aside class="w-52 bg-yellow-500 p-4">
+				<!-- <MenuLateral {menus} /> -->
+			</aside>
+			<!-- Main Content -->
+			<main class="space-y-4 bg-green-500 p-4">
+				{@render children()}
+			</main>
+		</div>
+		<!-- Footer -->
+		<footer class="bg-blue-500 p-4">EVG sistemas, todos os direitos reservados</footer>
 	</div>
-	<!-- ---- / ---- -->
-	<!-- (pageFooter) -->
-	<!-- (footer) -->
-</AppShell>
+</ToastProvider>
